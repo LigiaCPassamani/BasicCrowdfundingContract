ead4c47f1c4b3a5f4cb7569d756ffd145d3f1329
Ligia Passamani <ligiacpassamani@gmail.com>
2022-12-26 11:45:57 -0300

    Basic Solidity Smart Contract

---
+++ Campaign.sol
@@ -0,0 +1,109 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+library SafeMath {
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a / b;
+    return c;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract CampaignFactory {
+    address payable[] public deployedCampaigns;
+
+    function createCampaign(uint minimum) public{
+        address newCampaign = address(new Campaign(minimum, msg.sender));
+        deployedCampaigns.push(payable(newCampaign));
+    }
+
+    function getDeployedCampaigns() public view returns (address payable[] memory) {
+        return deployedCampaigns;
+    }
+}
+
+
+contract Campaign {
+    using SafeMath for uint256;
+
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+        uint approvalCount;
+        mapping(address => bool) approvals;
+    }
+
+    Request[] public requests;
+    address public manager;
+    uint public minimumContribution;
+    mapping(address => bool) public approvers;
+    uint public approversCount;
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+    constructor(uint minimum, address creator) {
+        manager = creator;
+        minimumContribution = minimum;
+    }
+
+    function contribute() public payable {
+       require(msg.value > minimumContribution);
+       approvers[msg.sender] = true;
+       approversCount = approversCount.add(1);
+    }
+
+    function createRequest(string memory description, uint value, address recipient) public restricted {
+        Request storage newRequest = requests.push(); 
+        newRequest.description = description;
+        newRequest.value= value;
+        newRequest.recipient= recipient;
+        newRequest.complete= false;
+        newRequest.approvalCount= 0;
+    }
+
+    function approveRequest(uint index) public {
+        Request storage request = requests[index];
+
+        require(approvers[msg.sender]);
+        require(!request.approvals[msg.sender]);
+
+        request.approvals[msg.sender] = true;
+        request.approvalCount = request.approvalCount.add(1);
+    }
+
+    function finalizeRequest(uint index) public restricted {
+        Request storage request = requests[index];
+
+        require(request.approvalCount > (approversCount.div(2)));
+        require(!request.complete);
+
+        payable(request.recipient).transfer(request.value);
+        request.complete = true;
+    }
+
+    function getSummary() public view returns (
+      uint, uint, uint, uint, address
+      ) {
+        return (
+          minimumContribution,
+          address(this).balance,
+          requests.length,
+          approversCount,
+          manager
+        );
+    }
+    
+    function getRequestsCount() public view returns (uint) {
+        return requests.length;
+    }
+}
